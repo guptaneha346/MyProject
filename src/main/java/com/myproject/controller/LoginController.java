3131b36e362f30e67c46232d51a81470bf3e2992
guptaneha346 <neha.gupta@impressico.com>
2019-06-10 17:24:10 +0530

    springboot project

--- src/main/java/com/myproject/controller/LoginController.java
+++ src/main/java/com/myproject/controller/LoginController.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -75,6 +78,19 @@ public class LoginController {
     }
 
 
+         @GetMapping(value = "/search")
+         public  String search(Model model, @RequestParam(defaultValue = "") String name){
+        model.addAttribute("userList",userService.findByName(name));
+        return "user/user_list";
+
+}
+
+    @GetMapping(value = "/search1")
+    public  String search1(Model model, @RequestParam(defaultValue = "") String name){
+        model.addAttribute("userList",userService.findByName(name));
+        return "user/Show_list";
+
+    }
 
     @RequestMapping(value= {"/home"}, method=RequestMethod.GET)
     public ModelAndView home() {
